974c46cfa6f9c93045b305319ecda75fd21dc23c
Jay <jccruz009@yahoo.com>
2024-04-01 21:06:31 +0800

    feat(main.js): create webscraper script

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,40 @@
-function removeDaggerSymbol(list) {
-  console.warn("attempt to make contact", list);
+const allList = document.querySelectorAll("li");
+const allSup = document.querySelectorAll("sup");
+
+// const toggle = document.getElementById("toggle");
+// let toggleValue = false;
+// toggle.addEventListener("change", (event) => {
+//   toggleValue = event.target.value;
+// });
+
+function removeDaggerSymbol(
+  list, 
+  // toggle = true
+) {
   if (list) {
     list.forEach((el) => {
       if (el.innerHTML.includes("\u{2020}")) {
-        el.innerHTML = el.innerHTML.replace("\u{2020}", "");
+        el.innerHTML = el.innerHTML.replace(
+          "\u{2020}",
+          '<span style="visibility: hidden">&#x2020;</span>',
+        );
+        // if (toggle) {
+        //   el.innerHTML = el.innerHTML.replace(
+        //     "\u{2020}",
+        //     '<span style="visibility: hidden">&#x2020;</span>',
+        //   );
+        // } else {
+        //   el.innerHTML = el.innerHTML.replace(
+        //     /<span class="visibility: hidden">&#x2020;<\/span>/gu,
+        //     "\u{2020}",
+        //   );
+        // }
       }
     });
   }
 }
 
-const allList = document.querySelectorAll("li");
-const allSup = document.querySelectorAll("sup");
-removeDaggerSymbol(allList);
-removeDaggerSymbol(allSup);
+window.addEventListener("load", () => {
+  removeDaggerSymbol(allList);
+  removeDaggerSymbol(allSup);
+});
